7841e2a16624874383faef5a9130c1d5e74d2934
Amit Alon <amit@kaholo.io>
2021-08-11 19:30:46 +0300

    removed verify trigger - and renamed priority to severity

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Kaholo trigger for integration with Okta Event Hooks.
 
 ## How To Use
 After installing the trigger on your kaholo server, create a new Event hook to this trigger's webhook in Okta admin console. Make sure to verify the event hook on Okta(which will send a verify request to the webhook on Kahlo). You can see more on creating event hooks [here](https://help.okta.com/en/prod/Content/Topics/automation-hooks/event-hooks-main.htm).
+**You can't verify the webhook of this trigger using Okta, until the trigger is installed on your Kaholo Server!**
 
 ## Webhook URL
 \<Kaholo-Server-URL\>/webhook/okta/event
@@ -14,9 +15,4 @@ Triggers when an event is sent to this method's webhook URL using POST HTTP meth
 1. Event Types Patterns (Text) **Optional** - The event types to catch in this trigger. Can specify multiple event types by seperating each with a new line.
 2. Event Severity (Options) **Optional** - The alert severity to catch in the trigger. Possible values: Any | Debug | Info | Warning | Error | Fatal.
 Default Value is Any.
-3. Include Higher Priority (Boolean) **Optional** - If true, also accept any events with a higher priority than the one specified in event severity. *Only relevant in case severity is not any.*
-
-## Method: Verify
-Triggers after a verify request to the event hook is sent from Okta using the webhook URL's and GET HTTP method.
-
-### Parameters: None
+3. Include Higher Severity (Boolean) **Optional** - If true, also accept any events with a higher severity than the one specified in event severity. *Only relevant in case severity is not any.*
--- src/app.js
+++ src/app.js
@@ -8,14 +8,14 @@ async function alert(req, res, settings, triggerControllers) {
       const eventType = event.eventType;
       const severity = parsers.severity(event.severity);
       triggerControllers.forEach((trigger) => {
-        let {alertEventTypes, alertSeverity, includeHigherPriority} = trigger.params;
+        let {alertEventTypes, alertSeverity, includeHigherSeverity} = trigger.params;
         alertEventTypes = alertEventTypes ? alertEventTypes.split("\n").map(eType => eType.trim()) : undefined;
         alertSeverity = parsers.severity(alertSeverity);
   
         if (alertEventTypes && !alertEventTypes.some(eType => micromatch.isMatch(eventType, eType))) return;
         if (alertSeverity !== "Any"){
           if (alertSeverity > severity) return;
-          if (!includeHigherPriority && alertSeverity != severity) return;
+          if (!includeHigherSeverity && alertSeverity != severity) return;
         }
         trigger.execute(`Okta Event: ${eventType} ${event.displayMessage}`, event);
       });
@@ -27,20 +27,13 @@ async function alert(req, res, settings, triggerControllers) {
   }
 }
 
-async function verify(req, res, settings, triggerControllers) {
-  try {
-    const reqChanllenge = req.headers['x-okta-verification-challenge'];
-    if (reqChanllenge){
-      res.status(200).send({"verification" : reqChanllenge});
-      triggerControllers.forEach((trigger) => {
-        trigger.execute(`Okta Verified Event Hook`, req.body || {});
-      });
-      return "Okta Event Hook Verified";
-    }
-    res.status(422).send("Unknown Request Format");
+async function verify(req, res) {
+  const reqChanllenge = req.headers['x-okta-verification-challenge'];
+  if (reqChanllenge){
+    res.status(200).send({"verification" : reqChanllenge});
   }
-  catch (err){
-    res.status(422).send(err.message || JSON.stringify(err));
+  else {
+    res.status(422).send("Unknown Request Format");
   }
 }
 
